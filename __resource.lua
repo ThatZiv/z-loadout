414fb9963b18fec97374d487313209fb21ac5c34
ThatZiv <35277113+ThatZiv@users.noreply.github.com>
2018-08-19 20:19:10 -0400

    Fixed Menu Issue

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,5 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Loadout Script by Zua'
 
-client_script 'zua.lua'
-
+client_scripts {
+    '@warmenu/warmenu.lua', 'zua.lua' }
